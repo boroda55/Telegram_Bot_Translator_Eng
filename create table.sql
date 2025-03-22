269befce376a26374ecf8a18d5aae002dfd4334e
Kirill <Kirill0098@gmail.com>
2025-03-23 01:00:21 +0500

    add create db

--- create table.sql
+++ create table.sql
@@ -6,11 +6,15 @@ create table if not exists Users(
 
 create table if not exists Words(
 	WordID serial primary key,
-	Word varchar(40) not null
+	Word_eng varchar(150) not null,
+	Word_rus varchar(150) not null
 );
-
 
 create table if not exists Studied_users(
 	UserID integer NOT NULL REFERENCES Users(UserID),
 	WordID integer NOT NULL REFERENCES Words(WordID)
 );
+
+--drop table Words;
+drop table users;
+drop table Studied_users;
